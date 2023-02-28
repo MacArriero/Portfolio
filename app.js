c5979b13a53e8fe787fd76b99b46a98948b4599c
MacArriero <s3812132@student.rmit.edu.au>
2023-02-28 18:08:05 +1100

    home dynamic text

--- app.js
+++ app.js
@@ -10,3 +10,22 @@ const observer = new IntersectionObserver((elements) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((element) => observer.observe(element));
+
+const text = document.querySelector(".dynamic-text");
+const textLoad = () => {
+    setTimeout(() => {
+        text.textContent = "Coder";
+    }, 0);
+    setTimeout(() => {
+        text.textContent = "Software Developer";
+    }, 4000);
+    setTimeout(() => {
+        text.textContent = "CompSci Graduate";
+    }, 8000);
+    setTimeout(() => {
+        text.textContent = "Programmer";
+    }, 12000);
+}
+
+textLoad();
+setInterval(textLoad, 12000);
