4e5fe05069eacbb8c145ecbe8703a3e792a33da0
MacArriero <s3812132@student.rmit.edu.au>
2023-02-28 14:48:10 +1100

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const observer = new IntersectionObserver((elements) => {
+    elements.forEach((element) => {
+        if(element.isIntersecting){
+            element.target.classList.add('show');
+        } else {
+            element.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((element) => observer.observe(element));
