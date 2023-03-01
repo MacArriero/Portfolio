3e709fd649d8e802fa5cd1627faa0a0116fc3ed8
MacArriero <s3812132@student.rmit.edu.au>
2023-03-01 17:04:20 +1100

    completed contact me section

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Checks if element can be seen on the client side
 const observer = new IntersectionObserver((elements) => {
     elements.forEach((element) => {
         if(element.isIntersecting){
@@ -11,6 +12,7 @@ const observer = new IntersectionObserver((elements) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((element) => observer.observe(element));
 
+// Changes the text value for the typing animation
 const text = document.querySelector(".dynamic-text");
 const textLoad = () => {
     setTimeout(() => {
@@ -26,6 +28,23 @@ const textLoad = () => {
         text.textContent = "Programmer";
     }, 12000);
 }
-
 textLoad();
 setInterval(textLoad, 12000);
+
+// Sends email from client side
+emailjs.init('obtEvVRYz6LJbg3Xg');
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        emailjs.sendForm('service_x46fr4x', 'template_bhhy00n', this)
+        .then(function() {
+            alert('Thank you! Your message was successfully sent.');
+            document.getElementById("contact-form").reset();
+        }, function(error) {
+            alert('Error with sending the message - ', error);
+        });
+        
+    });
+    
+}
+
